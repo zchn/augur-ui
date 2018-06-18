91fae15b791ccbaaa8e1546cf868aef0bc686c72
Justin Barry <328965+justinbarry@users.noreply.github.com>
2018-06-18 16:06:25 -0700

    Use dev build for integration tests

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -4,6 +4,10 @@ module.exports = {
     headless: process.env.HEADLESS !== 'false',
     serial: true,
     executablePath: 'chromium-browser',
-    args: ['--disable-gpu', '--disable-software-rasterizer', '--headless', '--mute-audio', '--hide-scrollbars', '--remote-debugging-port=9222']
+    args: ['--disable-gpu', '--disable-software-rasterizer', '--headless', '--mute-audio', '--hide-scrollbars', '--remote-debugging-port=9222', '--no-sandbox']
+  },
+  server: {
+    command: 'node server.js',
+    port: 8080,
   },
 }
